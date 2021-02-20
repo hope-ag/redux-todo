1d0fcdd72e0364bc669f2f0199e6847dd2a567e7
hope-ag <agyabahop3@gmail.com>
2021-02-20 11:53:32 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Todo app built with redux.
 
+<img src="todo.png" alt='demo'>
+
 Inspired by [angle943's redux-toolkit todo app](https://github.com/angle943/redux-toolkit-comparison)
+
+To use this for practice, switch to the ##starter## branch and run `yarn start` or `npm start` if that's what you use
--- src/features/actions.js
+++ src/features/actions.js
@@ -1,7 +1,56 @@
-export const actions = {
-  CREATE: "CREATE_TODO",
-  EDIT: "EDIT_TODO",
-  DELETE: "DELETE_TODO",
-  TOGGLE: "TOGGLE_TODO",
-  SELECT: "SELECT_TODO",
+import { v1 as uuid } from "uuid";
+const CREATE = "CREATE_TODO";
+const EDIT = "EDIT_TODO";
+const DELETE = "DELETE_TODO";
+const TOGGLE = "TOGGLE_TODO";
+const SELECT = "SELECT_TODO";
+
+export const createTodoActionCreator = (data) => {
+  const { desc } = data;
+  return {
+    type: CREATE,
+    payload: {
+      id: uuid(),
+      desc,
+      isComplete: false,
+    },
+  };
+};
+export const editTodoActionCreator = (data) => {
+  const { desc, id } = data;
+  return {
+    type: EDIT,
+    payload: {
+      id,
+      desc,
+    },
+  };
+};
+export const deleteTodoActionCreator = (data) => {
+  const { id } = data;
+  return {
+    type: DELETE,
+    payload: {
+      id,
+    },
+  };
+};
+export const toggleTodoActionCreator = (data) => {
+  const { id, isComplete } = data;
+  return {
+    type: TOGGLE,
+    payload: {
+      id,
+      isComplete,
+    },
+  };
+};
+export const selectTodoActionCreator = (data) => {
+  const { id } = data;
+  return {
+    type: SELECT,
+    payload: {
+      id,
+    },
+  };
 };
